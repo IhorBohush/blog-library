ec613c5e3eb0d3f5afe9af8ccf409fa7570a6931
IhorBohush <boigor95@gmail.com>
2025-03-27 00:13:11 +0200

    js. to menu lines

--- js/index.menu-line.js
+++ js/index.menu-line.js
@@ -1,14 +1,19 @@
 const content = document.querySelector('.header__menu-content');
 const line = document.querySelector('.header__menu-line');
 const item = document.querySelector('.header__menu-item');
-line.style.width = `0`
-item.style.color = 'var(--color-button-text)'
+line.style.width = `0`;
+item.style.color = 'var(--color-button-text)';
 
-function moveColor() {
-    item.style.color = `rgb(248, 118, 148)`;
-}
+// function moveColor() {
+//     item.style.color = `rgb(248, 118, 148)`;
+// }
 function moveLine() {
-    line.style.width = `30%`;
+    item.style.color = `rgb(248, 118, 148)`;
+    for (let i = 0; i < 30; i++) {
+        line.style.width = `${i}%`;
+        setTimeout(moveLine, 1000);
+        // await sleep(1);
+    }
     // const lineWidth = 0;
     // for (lineWidth; lineWidth < 30; lineWidth++) {
     //     line.style.width = lineWidth;
@@ -19,6 +24,6 @@ function exitLine() {
     item.style.color = `var(--color-button-text)`;
 }
 
-content.addEventListener('mouseover', moveColor, true);
-content.addEventListener('mouseover', moveLine, true);
-content.addEventListener('mouseout', exitLine, true);
+// item.addEventListener('mouseover', moveColor, true);
+item.addEventListener('mouseover', moveLine, true);
+item.addEventListener('mouseout', exitLine, true);
